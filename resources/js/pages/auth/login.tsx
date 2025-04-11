f73b3a2bdb9db455f152fed222f567afa2923f58
James Henry BSIT 2-C <jameshenrypavia.nobleza@bicol-u.edu.ph>
2025-04-11 11:27:12 +0800

    Framer Motion import

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -9,6 +9,7 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
+import { motion } from 'framer-motion';
 
 type LoginForm = {
     email: string;
