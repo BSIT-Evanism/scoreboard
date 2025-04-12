1ac00fd339efb0c364cca6ce839f97e4c422df4f
James Henry BSIT 2-C <jameshenrypavia.nobleza@bicol-u.edu.ph>
2025-04-12 13:29:51 +0800

    Update app-logo.tsx

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,11 +3,12 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
+            <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
                 <AppLogoIcon className="size-5" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Bicol University<br/></span>
+            <div className="ml-1 grid flex-1 text-left">
+                <span className="mb-0.5 truncate leading-none font-semibold text-sm">Bicol University<br/></span>
+                <span className="mb-0.5 truncate leading-none text-xs opacity-70">OVPRDE</span>
             </div>
         </>
     );
